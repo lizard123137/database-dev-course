888cf0a65c502e5de12a7eabdb1a7be7b76b2831
lizard123137 <lizard123137@gmail.com>
2025-03-29 12:16:27 +0100

    work during class

--- README.md
+++ README.md
@@ -7,3 +7,14 @@ For the project I am using `PostgreSQL` for the `sql` section and `MongoDB` for 
 The project documentation is written in the Polish language, as specified, by the course requirements.
 
 The course materials are publicly available [here](https://dsc.pollub.pl/sql).
+
+### Running the project
+
+Both of the sections have a `Dockerfile` inside of them.
+
+Running the `SQL` module:
+``` bash
+cd sql
+docker build --tag 'database-dev-sql' .
+docker run 'database-dev-sql'
+```
--- sql/init_all.sql
+++ sql/init_all.sql
@@ -7,8 +7,57 @@
 \i /docker-entrypoint-initdb.d/schema/offers.sql
 \i /docker-entrypoint-initdb.d/schema/user_likes.sql
 
+
+
 \echo 'Loading initial data...'
-COPY manufacturers(name)
+COPY manufacturers(id,name)
 FROM '/docker-entrypoint-initdb.d/data/manufacturers.csv'
 DELIMITER ','
 CSV HEADER;
+SELECT setval('manufacturers_id_seq', (SELECT MAX(id) FROM manufacturers));
+
+COPY vehicles(id,manufacturer_id,model,generation,horse_power,left_hand_drive,driven_wheels,transmission,body_style)
+FROM '/docker-entrypoint-initdb.d/data/vehicles.csv'
+DELIMITER ','
+CSV HEADER;
+SELECT setval('vehicles_id_seq', (SELECT MAX(id) FROM vehicles));
+
+COPY user_details(id,avatar,description)
+FROM '/docker-entrypoint-initdb.d/data/user_details.csv'
+DELIMITER ','
+CSV HEADER;
+SELECT setval('user_details_id_seq', (SELECT MAX(id) FROM user_details));
+
+COPY user_stats(id,cars_sold,cars_bought)
+FROM '/docker-entrypoint-initdb.d/data/user_stats.csv'
+DELIMITER ','
+CSV HEADER;
+SELECT setval('user_stats_id_seq', (SELECT MAX(id) FROM user_stats));
+
+COPY users(id,email,username,password,user_details_id,user_stats_id,created_at,modified_at)
+FROM '/docker-entrypoint-initdb.d/data/users.csv'
+DELIMITER ','
+CSV HEADER;
+SELECT setval('users_id_seq', (SELECT MAX(id) FROM users));
+
+COPY offers(id,user_id,vehicle_id,price,description,status,color,milage,vin,condition,created_at,modified_at)
+FROM '/docker-entrypoint-initdb.d/data/offers.csv'
+DELIMITER ','
+CSV HEADER;
+SELECT setval('offers_id_seq', (SELECT MAX(id) FROM offers));
+
+COPY user_likes(id,user_id,offer_id)
+FROM '/docker-entrypoint-initdb.d/data/user_likes.csv'
+DELIMITER ','
+CSV HEADER;
+SELECT setval('user_likes_id_seq', (SELECT MAX(id) FROM user_likes));
+
+
+
+\echo 'Example Queries:'
+\echo ' - DML Queries:'
+INSERT INTO manufacturers(name) VALUES ('volvo');
+UPDATE manufacturers SET name = 'Volvo' WHERE name = 'volvo';
+
+INSERT INTO manufacturers(name) VALUES ('test');
+DELETE FROM manufacturers WHERE name = 'test';
